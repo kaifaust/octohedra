0e2b76bd9b2712b14c26fa7999566e55db2b4cb6
Jeremy Silver <jeremysilvertongue@gmail.com>
2021-09-02 08:44:38 -0700

    Removed dead SCAD code

--- Octoflake/analysis/printing/OctoGrid.py
+++ Octoflake/analysis/printing/OctoGrid.py
@@ -203,30 +203,6 @@ class OctoGrid:
 
         self.four_way()
 
-    # def render_to_scad(self, cell_iteration, cell_size, overlap, slit):
-    #     out = ["use <octomodules.scad>"]
-    #     self.compute_trimming()
-    #
-    #     for center in sorted(self.occ, key=lambda x: x[2]):
-    #         cell = self.occ[center]
-    #         center_str = f"[{center[0] / 2 * cell_size}, {center[1] / 2 * cell_size}, {center[2] * cell_size * sqrt22}]"
-    #         out.append(f"translate({center_str}) {cell.render_scad(cell_iteration, cell_size, overlap, slit)}")
-    #         out.append(f"translate({center_str}) {cell.render_scad(cell_iteration, cell_size, overlap, slit)}")
-    #
-    #     for center, is_pyramid in self.welding.items():
-    #         center_str = f"[{center[0] / 2 * cell_size}, {center[1] / 2 * cell_size}, {center[2] * cell_size * sqrt22}]"
-    #         welding_size = round(4 * overlap + slit, 10)
-    #         cube_str = f"translate({center_str}) "
-    #         cube_str += f"translate([0, 0, {welding_size / 2}]) " if is_pyramid else "translate([0, 0, 0]) "
-    #         cube_str += f"cube([{welding_size}, {welding_size}, {welding_size}], center=true);"
-    #         out.append(cube_str)
-    #
-    #
-    #     return "\n".join(out)
-    #
-    # def render_to_stl(self):
-    #     pass
-
 
 class Trim(Enum):
     TOP = 1  # Unused
@@ -248,33 +224,6 @@ class OctoCell:
         self.is_pyramid = is_pyramid
         self.trims = set()
 
-    def render_scad(self, iteration, size, overlap, slit):
-        rounding = 10
-        size = round(size + 2 * overlap, rounding)
-        trim = round(overlap + slit / 2, rounding)
-        overlap = round(overlap, rounding)
-        slit = round(slit, rounding)
-
-        trim_front = trim if Trim.FRONT in self.trims else 0
-        trim_back = trim if Trim.BACK in self.trims else 0
-        trim_left = trim if Trim.LEFT in self.trims else 0
-        trim_right = trim if Trim.RIGHT in self.trims else 0
-        trim_bottom = round(sqrt2 * overlap, rounding)
-
-        kern_front_left = trim if Trim.FRONT_LEFT in self.trims else 0
-        kern_front_right = trim if Trim.FRONT_RIGHT in self.trims else 0
-        kern_back_left = trim if Trim.BACK_LEFT in self.trims else 0
-        kern_back_right = trim if Trim.BACK_RIGHT in self.trims else 0
-
-        is_pyramid = str(self.is_pyramid).lower()
-
-        return f"octahedron({iteration=}, {size=}, {overlap=}, {slit=},\n" \
-               f"    {trim_front=}, {trim_back=}, {trim_left=}, {trim_right=}, {trim_bottom=},\n" + \
-               f"    {kern_front_left=}, {kern_front_right=}, {kern_back_left=}, {kern_back_right=}, {is_pyramid=!s});"
-        # return f"octahedron({iteration=}, {size=});"#, {size=}, {overlap=}, {slit=},{is_pyramid=!s});\n" \
-        #        # f"    {trim_front=}, {trim_back=}, {trim_left=}, {trim_right=}, {trim_bottom=},\n" + \
-        #        # f"    {kern_front_left=}, {kern_front_right=}, {kern_back_left=}, {kern_back_right=}, {is_pyramid=!s});"
-
     def __str__(self):
         return f"Octahedron({self.trims})"
 
@@ -282,56 +231,6 @@ class OctoCell:
         return self.__str__()
 
 
-def tower(grid, iteration, center=(0, 0, 0)):
-    z = center[2]
-    for i in range(iteration, 0, -1):
-        print(i)
-        grid.make_flake(i, (center[0], center[1], center[2] + z))
-        z += 2 ** i
 
 
-def tower_complex(grid, iteration, center=(0, 0, 0)):
-    if iteration < 3:
-        return
-    tower(grid, iteration, center)
-    tower_complex(grid, iteration - 1, (center[0] + 2 ** iteration, center[1] + 2 ** iteration, center[2]))
-    tower_complex(grid, iteration - 1, (center[0] + 2 ** iteration, center[1] - 2 ** iteration, center[2]))
-    tower_complex(grid, iteration - 1, (center[0] - 2 ** iteration, center[1] + 2 ** iteration, center[2]))
-    tower_complex(grid, iteration - 1, (center[0] - 2 ** iteration, center[1] - 2 ** iteration, center[2]))
 
-
-# grid = OctoGrid()
-#
-# iteration = 5
-# cell_iteration = 0
-# spacing = 1.5 * (2 ** cell_iteration)
-# slit = .1
-# overlap = 0.3 * sqrt22 * sqrt22 + .001
-# grid.make_flake(iteration, (0, 0, 0))
-# # grid.stellate(iteration-2, offset=2 ** iteration)
-#
-# # tower_complex(grid, 4)
-# grid.crop(z_min=0)
-
-
-# ################################################ Robin's lamp
-# taper = 2 ** (iteration-2)
-# cyl_top = (2/4 * 2 ** (iteration) - taper) * sqrt22 * spacing
-# cyl_bot = ( 3/4*  2 ** (iteration) - taper) * sqrt22 * spacing
-# cyl_radius = (1+ 2 ** (iteration - 3)) * spacing * sqrt2
-#
-# print(cyl_radius)
-#
-# rendered += f"\n translate([0, 0, {-cyl_bot}]) cylinder({ cyl_top + cyl_bot}, r= {cyl_radius}, $fn=200 );"
-# rendered += f"\ntranslate([0, 0, {cyl_top}]) cylinder({ taper * sqrt22 * spacing}, {cyl_radius}, 0, $fn=200 );"
-
-
-# rendered = grid.render_to_scad(cell_iteration, spacing, overlap, slit)
-#
-# with open("render.scad", "w") as render_file:
-#     render_file.writelines(rendered)
-#
-# subprocess.call(["OpenSCAD", "-o", os.path.expanduser("~/Desktop/render_bin.stl"),
-#                  "--enable=lazy-union",
-#                  "--export-format", "binstl",
-#                  "render.scad"])
