8e08f758b96f617fb069e6ad7b15f09df404e025
Jeremy Silver <jeremysilvertongue@gmail.com>
2021-10-15 18:22:58 -0700

    Checking in before I rotate everything by 45 degrees

--- Octoflake/analysis/printing/Stars.py
+++ Octoflake/analysis/printing/Stars.py
@@ -4,50 +4,77 @@ from printing.octo.builder.OctoBuilder import OctoBuilder
 from printing.octo.builder.OctoTowerBuilder import OctoTowerBuilder
 from printing.rendering import RenderUtils
 
-i = 5
+# i = 6
 
-single_builder = OctoBuilder()
-single_builder.make_flake(i, (0, 0, 0))
-single_mesh = single_builder.materialize()
-
-config = OctoConfigs.config_25
-config.absolute_layers_per_cell = 8
-config.derive()
-
-RenderUtils.basic_render(single_mesh, config, filename=f"{8}_single.stl")
-
-star_builder = OctoBuilder()
-star_builder.stellate(i, (0, 0, 0))
-star_mesh = star_builder.materialize()
-
-tower_builder = OctoTowerBuilder()
-tower_builder.simple_tower(i, (0, 0, p2(i)-p2(i-1)))
-tower_mesh = tower_builder.materialize()
-
-flower_builder = OctoTowerBuilder()
-flower_builder.flower_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
-flower_mesh = flower_builder.materialize()
-
-evil_builder = OctoTowerBuilder()
-evil_builder.evil_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
-evil_mesh = evil_builder.materialize()
-
-
-config = OctoConfigs.config_25
+# single_builder = OctoBuilder()
+# single_builder.make_flake(i, (0, 0, 0))
+# single_mesh = single_builder.materialize()
+#
+# config = OctoConfigs.config_25
+# config.absolute_layers_per_cell = 8
+# config.derive()
+#
+# # RenderUtils.basic_render(single_mesh, config, filename=f"{8}_single.stl")
+#
+# star_builder = OctoBuilder()
+# star_builder.stellate(i, (0, 0, 0))
+# star_mesh = star_builder.materialize()
+#
+# # RenderUtils.basic_render(single_mesh, config, filename=f"{8}_single.stl")
+#
+# tower_builder = OctoTowerBuilder()
+# tower_builder.simple_tower(i, (0, 0, p2(i)-p2(i-1)))
+# tower_mesh = tower_builder.materialize()
+#
+# flower_builder = OctoTowerBuilder()
+# flower_builder.flower_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
+# flower_mesh = flower_builder.materialize()
+#
+# evil_builder = OctoTowerBuilder()
+# evil_builder.evil_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
+# evil_mesh = evil_builder.materialize()
+#
+#
+# config = OctoConfigs.config_25
 
 # RenderUtils.basic_render(builder.materialize(), config)
 
 
-# for layers in range(6, 7):
-#     config = OctoConfigs.config_25
-#     config.absolute_layers_per_cell = layers
-#     config.derive()
-#
-#     RenderUtils.basic_render(single_mesh, config, filename=f"{layers}_single.stl")
-#     RenderUtils.basic_render(star_mesh, config, filename=f"{layers}_star.stl")
-#     RenderUtils.basic_render(tower_mesh, config, filename=f"{layers}_tower.stl")
-#     RenderUtils.basic_render(flower_mesh, config, filename=f"{layers}_flower.stl")
-#     RenderUtils.basic_render(evil_mesh, config, filename=f"{layers}_evil.stl")
+for i in range(3, 6):
+    for layers in range(5, 9):
+        config = OctoConfigs.config_25
+        config.absolute_layers_per_cell = layers
+        config.derive()
+
+        single_builder = OctoBuilder()
+        single_builder.make_flake(i, (0, 0, 0))
+        single_mesh = single_builder.materialize()
+
+        # RenderUtils.basic_render(single_mesh, config, filename=f"{8}_single.stl")
+
+        star_builder = OctoBuilder()
+        star_builder.stellate(i, (0, 0, 0))
+        star_mesh = star_builder.materialize()
+
+        # RenderUtils.basic_render(single_mesh, config, filename=f"{8}_single.stl")
+
+        tower_builder = OctoTowerBuilder()
+        tower_builder.simple_tower(i, (0, 0, p2(i)-p2(i-1)))
+        tower_mesh = tower_builder.materialize()
+
+        flower_builder = OctoTowerBuilder()
+        flower_builder.flower_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
+        flower_mesh = flower_builder.materialize()
+
+        evil_builder = OctoTowerBuilder()
+        evil_builder.evil_tower(i, center=(0, 0, p2(i) - p2(i, - 2)), min_evil=3)
+        evil_mesh = evil_builder.materialize()
+
+        RenderUtils.basic_render(single_mesh, config, filename=f"{i}_{layers}_single.stl")
+        RenderUtils.basic_render(star_mesh, config, filename=f"{i}_{layers}_star.stl")
+        RenderUtils.basic_render(tower_mesh, config, filename=f"{i}_{layers}_tower.stl")
+        RenderUtils.basic_render(flower_mesh, config, filename=f"{i}_{layers}_flower.stl")
+        RenderUtils.basic_render(evil_mesh, config, filename=f"{i}_{layers}_evil.stl")
 
 
 
