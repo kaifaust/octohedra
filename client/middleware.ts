16810e5c12a1caff6f607de65a2289f54c34df6a
Kai Faust <kaifaust@gmail.com>
2025-12-26 13:51:43 -0600

    Strip params from social media

---
+++ client/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const { pathname, search } = request.nextUrl;
+
+  // Strip query params from opengraph-image and twitter-image routes
+  // Facebook and other crawlers append cache-busting params that can cause issues
+  if (
+    (pathname.endsWith('/opengraph-image') || pathname.endsWith('/twitter-image')) &&
+    search
+  ) {
+    const url = request.nextUrl.clone();
+    url.search = '';
+    return NextResponse.redirect(url, 308);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/r/:recipe/opengraph-image',
+    '/r/:recipe/twitter-image',
+    '/s/:id/opengraph-image',
+    '/s/:id/twitter-image',
+    '/opengraph-image',
+    '/twitter-image',
+  ],
+};
