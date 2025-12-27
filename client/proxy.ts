cb0f41ee087d5ff01a72bac3f970ee7a8e017b5f
Kai Faust <kaifaust@gmail.com>
2025-12-26 19:09:03 -0600

    Rename middleware to proxy

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname, search } = request.nextUrl;
 
   // Strip query params from opengraph-image and twitter-image routes
